a31b6365cc55ea9ab39d4b1020d144942ac53c46
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2025-04-24 00:38:12 +0300

    Add mapping for neighborhoods

--- cellular_automata/neighborhoods.py
+++ cellular_automata/neighborhoods.py
@@ -21,3 +21,10 @@ MOORE_1 = np.array(_MOORE_1, dtype=np.int8)
 MOORE_2 = np.array(_MOORE_2, dtype=np.int8)
 VON_NEUMANN_1 = np.array(_VON_NEUMANN_1, dtype=np.int8)
 VON_NEUMANN_2 = np.array(_VON_NEUMANN_2, dtype=np.int8)
+
+NEIGHBORHOODS = {
+    "moore_1": MOORE_1,
+    "moore_2": MOORE_2,
+    "von_neumann_1": VON_NEUMANN_1,
+    "von_neumann_2": VON_NEUMANN_2,
+}
