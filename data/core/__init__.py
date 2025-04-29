64f14123faf88883b5e1f1d39e19d6932837a6ff
Nikita Lenyk <nikitalenyk2@gmail.com>
2025-04-29 15:22:28 +0300

    Add DataManager and Downloader to core module exports

--- data/core/__init__.py
+++ data/core/__init__.py
@@ -1,8 +1,11 @@
-# Core data processing modules
 from data.core.quantize_cpu import CPUStateDownSampler
 from data.core.quantize_gpu import CUDAStateDownSampler
+from data.manager.data_manager import DataManager
+from data.manager.downloader import Downloader
 
 __all__ = [
     "CPUStateDownSampler",
     "CUDAStateDownSampler",
+    "Datamanager",
+    "Downloader"
 ]
