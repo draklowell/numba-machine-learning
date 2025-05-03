eaa1511ef5d6c72e8be127091ef857d5bbd5ca95
Nikita Lenyk <nikitalenyk2@gmail.com>
2025-05-03 13:55:58 +0300

    fix init

--- loader/core/__init__.py
+++ loader/core/__init__.py
@@ -1,6 +1,4 @@
 from loader.core.quantize_cpu import CPUStateDownSampler
 from loader.core.quantize_gpu import CUDAStateDownSampler
-from loader.manager.data_manager import DataManager
-from loader.manager.downloader import Downloader
 
-__all__ = ["CPUStateDownSampler", "CUDAStateDownSampler", "DataManager", "Downloader"]
+__all__ = ["CPUStateDownSampler", "CUDAStateDownSampler"]
